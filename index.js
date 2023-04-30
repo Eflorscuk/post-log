5cc6861cb6690fd066e04ffa04c208a3d6bdd51a
Evandro Florscuk <evandro_ff@me.com>
2023-04-30 01:00:09 -0300

    Form Created

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const Sequelize = require('sequelize')
 const port = 8084
 
 // Template Engine
-app.engine('handlebars', engine())
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 // DB Conn
 // const sequelize = new Sequelize()
 
+// Routes
+app.get('/register', (req, res, next) => {
+    res.render('form')
+})
 
 app.listen(port, _ => console.log(`Listening on port ${port}!`))
