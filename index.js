d7182ff4e53dcca2ee8f53374fa63e473d63242d
Evandro Florscuk <evandro_ff@me.com>
2023-04-30 00:42:57 -0300

    Handlebars configured and Sequelize started

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const app = express()
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 const port = 8084
+
+// Template Engine
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// DB Conn
+const sequelize = new Sequelize()
+
 
 app.listen(port, _ => console.log(`Listening on port ${port}!`))
