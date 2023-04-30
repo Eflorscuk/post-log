b55a11653611dd4c1f889fe935aaa9f36498ce87
Evandro Florscuk <evandro_ff@me.com>
2023-04-30 00:52:05 -0300

    Bug fixed on Handlebars

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const app = express()
-const handlebars = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const Sequelize = require('sequelize')
 
 const port = 8084
 
 // Template Engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 // DB Conn
-const sequelize = new Sequelize()
+// const sequelize = new Sequelize()
 
 
 app.listen(port, _ => console.log(`Listening on port ${port}!`))
