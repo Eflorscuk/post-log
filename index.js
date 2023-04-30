1202e98e3861011ae5d0e17e05cbd451e697390c
Evandro Florscuk <evandro_ff@me.com>
2023-04-30 02:03:08 -0300

    Body-parser instaled, route /add created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const { engine } = require('express-handlebars')
 const Sequelize = require('sequelize')
+const bodyParser = require('body-parser')
 
 const port = 8084
 
@@ -10,12 +11,20 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // DB Conn
 // const sequelize = new Sequelize()
 
 // Routes
-app.get('/register', (req, res, next) => {
+app.get('/register', (req, res) => {
     res.render('form')
+})
+
+app.post('/add', (req, res) => {
+    res.send('Formulário recebido')
 })
 
 app.listen(port, _ => console.log(`Listening on port ${port}!`))
